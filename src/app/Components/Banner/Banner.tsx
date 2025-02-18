8b6d25800c8793e139c18096098cb77310b37795
hamzanasir123 <hk435981@gmail.com>
2025-02-18 12:12:21 +0500

    Removed Banner Video & Changed Font

--- src/app/Components/Banner/Banner.tsx
+++ src/app/Components/Banner/Banner.tsx
@@ -9,16 +9,6 @@ type BannerProps = {
 function Banner({ pageName }: BannerProps): React.JSX.Element {
   return (
     <div className="relative h-[250px] md:h-[300px] lg:h-[350px] overflow-hidden">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute inset-0 w-full h-full object-cover"
-      >
-        <source src="/Banner2.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
       <div className="absolute inset-0 bg-black bg-opacity-40"></div>
       <div className="relative flex flex-col items-center justify-center h-full text-white text-center">
 
